54b1d440cc6923d9bf80234027c3406bb90bc4ec
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-08-21 18:36:32 +0100

    add profile page and helper

---
+++ client/templates/profile/profile.js
@@ -0,0 +1,17 @@
+Template.profile.events({
+   'change .myFileInput'(event, tmpl) {
+       FS.Utility.eachFile(event, (file) => {
+          Images.insert(file, (error, fileObj) => {
+             if (!error) {
+                 const userId = Meteor.userId();
+                 const imageUrl = {
+                     'profile.avatar': '/cfs/files/images/' + fileObj._id
+                 };
+                 setTimeout(() => {
+                     Meteor.users.update(userId, {$set: imageUrl});
+                 });
+             }
+          });
+       });
+   }
+});
